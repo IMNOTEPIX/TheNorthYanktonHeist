6bbe699a0e014b5c27c49ecedc06fce5686b7c69
Froogiee <l.levandavicius@gmail.com>
2025-08-15 01:36:44 +0300

    ill add description later

--- BobcatSecurityDepotHeist/Functions.cs
+++ BobcatSecurityDepotHeist/Functions.cs
@@ -1345,7 +1345,6 @@ namespace BobcatSecurityDepotHeist
                     Script.Wait(0);
                 }
             }
-
             public void DrawShard()
             {
                 if (!Game.Player.Character.IsDead)
